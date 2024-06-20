ce436917ea65b32f33609a4fdb3d0c3d2ac036de
Chang Ya Ching <yaching35@gmail.com>
2024-06-20 21:49:39 +0800

    update indentation

--- src/input_processing.py
+++ src/input_processing.py
@@ -8,5 +8,5 @@ def format_model_input (input_dict):
         married = int(input_dict['married'])
         zip_code = int(input_dict['zip_code'])
         num_referrals = int(input_dict['num_referrals'])
-
-    return [tenure_months, total_monthly_fee, age, num_dependents, contract_type, payment_method, married, zip_code, num_referrals]
+        
+        return [tenure_months, total_monthly_fee, age, num_dependents, contract_type, payment_method, married, zip_code, num_referrals]
